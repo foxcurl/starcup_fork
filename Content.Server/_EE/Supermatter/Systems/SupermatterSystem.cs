ce77a28850118e74c14099186b86426196ace190
foxcurl <kitshoffeitt@gmail.com>
2025-09-02 19:26:20 -0400

    Merge pull request #2455 from Darkmajia/supermatter-delams
    THE ENDLESS TOIL PART 2 - more supermatter adjustments, delam expansion pack
    
    (cherry picked from commit a837ddcb7ddcbfc6703af349b4876d34e7200d82)

--- Content.Server/_EE/Supermatter/Systems/SupermatterSystem.cs
+++ Content.Server/_EE/Supermatter/Systems/SupermatterSystem.cs
@@ -6,9 +6,11 @@ using Content.Server.Chat.Systems;
 using Content.Server.DoAfter;
 using Content.Server.Examine;
 using Content.Server.Explosion.EntitySystems;
+using Content.Server.GameTicking;
 using Content.Server.Lightning;
 using Content.Server.Popups;
 using Content.Server.Radio.EntitySystems;
+using Content.Server.Silicons.Laws;
 using Content.Server.Singularity.Components;
 using Content.Server.Singularity.EntitySystems;
 using Content.Server.Traits.Assorted;
@@ -35,6 +37,7 @@ using Robust.Shared.Physics;
 using Robust.Shared.Physics.Components;
 using Robust.Shared.Physics.Events;
 using Robust.Shared.Player;
+using Robust.Shared.Prototypes;
 using Robust.Shared.Random;
 using Robust.Shared.Timing;
 
@@ -49,7 +52,9 @@ public sealed partial class SupermatterSystem : EntitySystem
     [Dependency] private readonly EntityLookupSystem _entityLookup = default!;
     [Dependency] private readonly ExamineSystem _examine = default!;
     [Dependency] private readonly ExplosionSystem _explosion = default!;
+    [Dependency] private readonly GameTicker _gameTicker = default!;
     [Dependency] private readonly GravityWellSystem _gravityWell = default!;
+    [Dependency] private readonly IonStormSystem _ionStorm = default!;
     [Dependency] private readonly LightningSystem _lightning = default!;
     [Dependency] private readonly ParacusiaSystem _paracusia = default!;
     [Dependency] private readonly PointLightSystem _light = default!;
@@ -64,6 +69,7 @@ public sealed partial class SupermatterSystem : EntitySystem
     [Dependency] private readonly IChatManager _chatManager = default!;
     [Dependency] private readonly IConfigurationManager _config = default!;
     [Dependency] private readonly IGameTiming _timing = default!;
+    [Dependency] private readonly IPrototypeManager _proto = default!;
     [Dependency] private readonly IRobustRandom _random = default!;
 
     public override void Initialize()
--- Resources/Locale/en-US/_Impstation/interaction/interaction-popup-component.ftl
+++ Resources/Locale/en-US/_Impstation/interaction/interaction-popup-component.ftl
@@ -2,4 +2,7 @@
 
 ## Petting animals
 
-petting-success-suppy = {THE($target)} crackles with energy as you pet one of {POSS-ADJ($target)} facets.
+petting-success-suppy = {CAPITALIZE(THE($target))} crackles with energy as you pet one of {POSS-ADJ($target)} facets.
+
+petting-success-smspider = You pet {THE($target)} and feel a wave of dread wash over you.
+petting-failure-smspider = You reach out to pet {THE($target)}, but decide against it.
