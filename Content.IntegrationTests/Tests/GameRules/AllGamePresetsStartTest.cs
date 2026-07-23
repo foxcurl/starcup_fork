c34064678626b1ce4429f10f523a8a64f8acdabb
Oxdeception <30361731+Oxdeception@users.noreply.github.com>
2026-07-23 09:51:42 -0400

    merge-fix: Ignore late start in tests
    Failing since there's no SC map loaded for the test.
    Should be reworked later.

--- Content.IntegrationTests/Tests/GameRules/AllGamePresetsStartTest.cs
+++ Content.IntegrationTests/Tests/GameRules/AllGamePresetsStartTest.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 using Content.IntegrationTests.Fixtures.Attributes;
 using Content.IntegrationTests.Utility;
@@ -22,7 +22,7 @@ public sealed class AllGamePresetsStartTest : AntagTest
     /// A list of blacklisted <see cref="GamePresetPrototype"/> for this test. Some down streams might make changes which nuke upstream game modes they don't use.
     /// This prevents them from being tested. If you use this to silence valid test fails and your game fails to start. Skill issue. Do 100 push-ups.
     /// </summary>
-    private static readonly HashSet<string> IgnoredPresets = []; // Is a string to prevent YAML Linter from freaking if this is empty.
+    private static readonly HashSet<string> IgnoredPresets = ["LateStart"]; // Is a string to prevent YAML Linter from freaking if this is empty.
 
     private static string[] _gamePresets = GameDataScrounger.PrototypesOfKind<GamePresetPrototype>().Where(p => !IgnoredPresets.Contains(p)).ToArray();
 
